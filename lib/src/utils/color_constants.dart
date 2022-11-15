5f6aa872731367806841a1bba524820091e17c91
PierreL <pierre.lafuma@gmail.com>
2022-11-15 01:06:23 +0100

    feat: Added color constants

---
+++ lib/src/utils/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static Color redSkilleos = const Color.fromARGB(255, 220, 38, 38);
+  static Color lightGrey = const Color.fromARGB(255, 240, 240, 240);
+  static Color darkGrey = const Color.fromARGB(255, 51, 51, 51);
+}
